071fbc4582b04b5bba0f6cb547abd83c4b78ecd0
Marvin Calderón <2327239740101@ingenieria.usac.edu.gt>
2019-05-04 13:55:39 -0600

    Cambio de bd

--- Develop/backend/Database/db.js
+++ Develop/backend/Database/db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql');
 
 var connection = mysql.createConnection({
     host     : 'basededatos-service', //ESTE ES EL NOMBRE DE CONTENEDOR EN EL AMBIENTE DE DESARROLLO
-    user     : process.env.DB_USER,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_NAME
+    user     : 'root',
+    password : 'root',
+    database : 'banco'
   });
 
 module.exports = connection;
