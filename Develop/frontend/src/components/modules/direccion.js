ff2b91ea5e2b83d8651e8b31e5b5f8fcedade392
Marvin Calderón <2327239740101@ingenieria.usac.edu.gt>
2019-05-04 10:31:23 -0600

    Cambio de ip

--- Develop/frontend/src/components/modules/direccion.js
+++ Develop/frontend/src/components/modules/direccion.js
@@ -2,5 +2,5 @@
 export default function getIp() {
     //return 'http://192.168.0.101:8000/';
     //return 'http://192.168.43.189:8000/';
-    return 'http://172.16.0.20:8000/';
+    return 'http://34.74.112.111:8000/';
 }
