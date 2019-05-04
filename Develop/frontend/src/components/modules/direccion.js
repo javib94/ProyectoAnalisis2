7b3dd76fa20aa15f1f1738e577097a9c0ede3be4
Marvin Calderón <2327239740101@ingenieria.usac.edu.gt>
2019-05-04 14:24:53 -0600

    Cambio de ip de backend

--- Develop/frontend/src/components/modules/direccion.js
+++ Develop/frontend/src/components/modules/direccion.js
@@ -2,5 +2,5 @@
 export default function getIp() {
     //return 'http://192.168.0.101:8000/';
     //return 'http://192.168.43.189:8000/';
-    return 'http://34.74.112.111:8081/';
+    return 'http://35.238.12.42:8000/';
 }
